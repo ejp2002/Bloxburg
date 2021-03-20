81bf44d1157401b28dc3d4159d6fad74683252b7
Spencer <36741900+Spencer-0003@users.noreply.github.com>
2021-03-20 14:39:33 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -32,10 +32,18 @@ end;
 local function getOrder(customer)
     if (not customer or (customer and not customer:FindFirstChild("Order"))) then return end;
 
-    local style = customer.Order:WaitForChild("Style").Value;
-    local colour = customer.Order:WaitForChild("Color").Value;
+    if (stats.Job.Value == "StylezHairdresser") then
+        local style = customer.Order:WaitForChild("Style").Value;
+        local colour = customer.Order:WaitForChild("Color").Value;
+    
+        return {style, colour};
+    elseif (stats.Job.Value == "BloxyBurgersCashier") then
+        local burger = customer.Order:WaitForChild("Burger").Value;
+        local fries = customer.Order:WaitForChild("Fries").Value;
+        local cola = customer.Order:WaitForChild("Cola").Value;
 
-    return {style, colour};
+        return {burger, fries, cola};
+    end;
 end;
 
 local bloxburg = library:CreateWindow("Bloxburg");
@@ -55,6 +63,35 @@ bloxburg:AddToggle({
 
         while library.flags.hairdresser do
             local workstations = workspace.Environment.Locations.StylezHairStudio.HairdresserWorkstations;
+            for i, v in next, workstations:GetChildren() do
+                if (v.Occupied.Value) then
+                    fireServer({
+                        Type = "FinishOrder";
+                        Workstation = v;
+                        Order = getOrder(v.Occupied.Value);
+                    });
+                end;
+            end;
+            wait();
+        end;
+    end;
+});
+
+bloxburg:AddToggle({
+    text = "Bloxy Burgers Cashier";
+    flag = "cashier";
+    callback = function(enabled)
+        if (not enabled) then return end;
+
+        if (stats.Job.Value ~= "BloxyBurgersCashier") then
+            jobManager:GoToWork("BloxyBurgersCashier"); --> TP to job
+        end;
+
+        repeat wait() until stats.Job.Value == "BloxyBurgersCashier";
+        tweenService:Create(client.Character.HumanoidRootPart, TweenInfo.new(0.75, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFrame.new(825.076355, 13.6776829, 276.091309, 0.0133343497, -5.09454665e-08, -0.999910772, 7.34347916e-09, 1, -5.08520621e-08, 0.999910772, -6.66474698e-09, 0.0133343497)}):Play();
+
+        while library.flags.cashier do
+            local workstations = workspace.Environment.Locations.BloxyBurgers.CashierWorkstations;
             for i, v in next, workstations:GetChildren() do
                 if (v.Occupied.Value) then
                     fireServer({
